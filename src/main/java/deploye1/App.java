d3913184c7341411b125c07f68e304521e71b8c4
EC2 Default User <ec2-user@ip-10-1-0-218.ec2.internal>
2020-12-19 05:53:01 +0000

    deploye1

---
+++ src/main/java/deploye1/App.java
@@ -0,0 +1,13 @@
+package deploye1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
